2f210cff0716a72294f841811dca4cc14430e769
colee1985 <5969226@qq.com>
2016-01-18 01:07:56 +0800

    已经可以用了

--- src/app/scripts/contentscript.js
+++ src/app/scripts/contentscript.js
@@ -1,3 +1,28 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+var btn = $('<div>').text('run').appendTo('body');
+btn.css({
+	position:'fixed',
+	right:0,
+	top:10,
+	zIndex:10000,
+	background: '#000',
+	color:'#fff',
+	fontSize:26
+});
+
+btn.click(function(){
+	var tr = $('tr[data-role="list-item"]');
+	tr.each(function(){
+		var ths = $(this);
+		var price = ths.find('span[data-role="bid-price-text"]').text();
+		price = parseFloat(price);
+		console.log(price);
+		if(price>0.35){
+			ths.find('input').eq(0).prop('checked', true);
+			ths.addClass('selected');
+		}
+	});
+});
